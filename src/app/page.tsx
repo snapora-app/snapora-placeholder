26789288e971f2e61ef51bb01540cf1841527892
Clik Admin <clikadmin@Adityaas-MacBook-Pro.local>
2025-02-19 22:20:07 +0530

    add copyright

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import Image from "next/image";
 
 export default function Home() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div
-      className="h-full flex flex-grow-0 justify-center center items-center"
+      className="h-full flex flex-col flex-grow-0 justify-center items-center"
     >
       <Image
         src={'/Snapora_Logo_Pre.png'} alt={'Logo'} width={512} height={512}
       />
+      <footer className="mt-4 text-center text-gray-600">
+        © {currentYear} snapora.app. All rights reserved.
+      </footer>
+
     </div>
   );
 }
